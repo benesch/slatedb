c87f943111ec41c43f76549abf0c8c282b1a9c58
Nikhil Benesch <nikhil.benesch@gmail.com>
2024-11-27 23:32:16 -0500

    [rebase me] validate gdb approach

--- slatedb-test-macros/src/lib.rs
+++ slatedb-test-macros/src/lib.rs
@@ -3,7 +3,7 @@ use quote::quote;
 use syn::{parse_macro_input, AttributeArgs, ItemFn, ReturnType};
 
 // A macro that runs a test (via `tokio::test`) after installing a signal
-// handler that will print backtraces for all async tasks if the test is
+// handler that will print backtraces for all threads via gdb if the test is
 // interrupted by SIGTERM. Useful for debugging tests that hang in CI.
 #[proc_macro_attribute]
 pub fn test(tokio_test_attrs: TokenStream, test_fn: TokenStream) -> TokenStream {
@@ -22,25 +22,34 @@ pub fn test(tokio_test_attrs: TokenStream, test_fn: TokenStream) -> TokenStream 
       #[tokio::test(#(#tokio_test_attrs)*)]
       #(#test_fn_attrs)*
       async fn #test_fn_name() #test_fn_ret {
-        #[async_backtrace::framed]
         async fn test_impl() #test_fn_ret {
           #test_fn_body
         }
 
-        let mut signal = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
-          .expect("test harness failed to create SIGTERM handler");
+        let sigterm_handler_task = ::std::env::var_os("CI").is_some().then(|| {
+          let mut signal = ::tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("test harness failed to create SIGTERM handler");
 
-        let sigterm_handler_task = tokio::spawn(async move {
-          signal.recv().await;
-          eprintln!(
-            "backtraces for running async tasks:\n{}",
-            async_backtrace::taskdump_tree(true),
-          );
-          std::process::abort();
+          ::tokio::spawn(async move {
+            signal.recv().await;
+            eprintln!("SIGTERM received; collecting backtraces from running threads with gdb...");
+            let pid = std::process::id();
+            ::std::process::Command::new("sudo")
+              .arg("gdb")
+              .arg("-p")
+              .arg(pid.to_string())
+              .arg("-ex")
+              .arg("thread apply all bt")
+              .spawn()
+              .expect("failed to spawn gdb")
+              .wait()
+              .expect("gdb failed");
+            std::process::abort();
+          })
         });
 
         let res = test_impl().await;
-        sigterm_handler_task.abort();
+        sigterm_handler_task.map(|task| task.abort());
         res
       }
     }
